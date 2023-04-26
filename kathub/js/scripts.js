573b5f349e837426bc1b862e00b3036d50cfe6e9
Katlego Maphango <mphangokatlego08@gmail.com>
2023-04-26 16:14:28 +0200

    hamburger menu toggle functionality

---
+++ kathub/js/scripts.js
@@ -0,0 +1,17 @@
+const toggleBtn = document.querySelector('[class="nav_toggle"]')
+const menuList = document.querySelector('[class="nav_links"]')
+const menuIcon = document.querySelector('.fa-solid')
+
+const menuHandler = () => {
+    console.log('pressed')
+    menuList.classList.toggle('nav_links-toggle')
+
+    console.log(toggleBtn.firstChild)
+    if(menuIcon.classList.contains("fa-bars")) {
+        menuIcon.classList.replace("fa-bars", "fa-x")
+    } else {
+        menuIcon.classList.replace("fa-x", "fa-bars")
+    }
+}
+
+toggleBtn.addEventListener('click', menuHandler)
