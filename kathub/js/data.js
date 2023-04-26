a7278e6ae4afcee39d8fb6e28644ef54203b2918
katlego Maphango <mphangokatlego08@gmail.com>
2023-04-26 19:06:36 +0200

    Update data.js

--- kathub/js/data.js
+++ kathub/js/data.js
@@ -2,21 +2,21 @@ export const projects = [
     {
         title: "ITW_Final Digital Resumé",
         description: "A responsive digitized version of my own Resume to demonstrate my learning of basic Web development. I designed the resume using figma and developed the resume using HTML and CSS.",
-        image: "/kathub/images/digital_resume.jpg", 
+        image: "./kathub/images/digital_resume.jpg", 
         sourceLink: "https://shorturl.at/nwGO8",
         liveLink: "https://shorturl.at/ayNQ6"
     },
     {
         title: "IWA_18.4: Kitchen Express",
         description: "A web App that tracks customer orders from when they are taken until they are served to the customer. It also records the table number and the exact time that the order was created.", 
-        image: "/kathub/images/kitchen_express.png",
+        image: "./kathub/images/kitchen_express.png",
         sourceLink: "",
         liveLink: ""
     },
     {
         title: "IWA19_Final Book Connect",
         description: "A web App that shows a list of Books, when a book is clicked a summary of the book is shown. The user can filter the books by the title, genre and/or author and the user can toggle between dark and light modes.",
-        image: "/kathub/images/book_connect.png", 
+        image: "./kathub/images/book_connect.png", 
         sourceLink: "https://shorturl.at/nwGO8",
         liveLink: "https://shorturl.at/ayNQ6"
     }
